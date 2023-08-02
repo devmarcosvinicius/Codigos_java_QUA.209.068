c6fe8a793457173e4669765ddbb782f55510bec0
Marcos Vinicius <contato@marcosviniciusdev>
2023-08-02 17:50:34 -0300

    first commit

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,19 @@
+package principal;
+
+// Classe principal
+public class OlaMundo {
+	
+	// metodo principal, onde executa o programa
+	public static void main(String[] args) {
+		// imprime uma linha e ao final salta uma nova linha
+		// print = imprimir; ln = new line
+		System.out.println("Hello world, Olá Mundo");
+		
+		// comentario de uma unica linha
+		
+		/*
+		 * comentario de multiplas linha
+		 */
+	}
+
+}
