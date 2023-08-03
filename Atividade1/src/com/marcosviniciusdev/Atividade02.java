bb9e742759ddef15d504853fdc2e09e4a91b8b9c
Marcos Vinicius <contato@marcosviniciusdev>
2023-08-03 15:25:15 -0300

    Adicionando comentarios.

--- Atividade1/src/com/marcosviniciusdev/Atividade02.java
+++ Atividade1/src/com/marcosviniciusdev/Atividade02.java
@@ -3,7 +3,7 @@ package com.marcosviniciusdev;
 public class Atividade02 {
 	
 	public static void main(String[] args) {
-		
+		// Crie duas variaveis com seu nome e sobrenome, depois imprimir concatenando na tela.
 		String primeiroNome = "Marcos";
 		String sobrenome = "Vinicius";
 		
