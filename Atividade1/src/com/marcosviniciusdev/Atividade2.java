c8ca292bed7d97bee031d0301da341a4dfe3651f
Marcos Vinicius <contato@marcosviniciusdev>
2023-08-03 14:37:47 -0300

    Enviando atividade 1 e atividade 2

---
+++ Atividade1/src/com/marcosviniciusdev/Atividade2.java
@@ -0,0 +1,13 @@
+package com.marcosviniciusdev;
+
+public class Atividade2 {
+	
+	public static void main(String[] args) {
+		
+		String primeiroNome = "Marcos";
+		String sobrenome = "Vinicius";
+		
+		System.out.println("Nome completo: " + primeiroNome + " " + sobrenome);
+	}
+
+}
---
+++ Atividade1/src/com/marcosviniciusdev/PrimeiraAtividade.java
@@ -0,0 +1,34 @@
+package com.marcosviniciusdev;
+
+public class PrimeiraAtividade {
+	
+	public static void main(String[] args) {
+		int numero1 = 5;
+		int numero2 = 4;
+		
+		System.out.println(numero1 + numero2);
+		
+		double double1 = 55.22;
+		double double2 = 44.82;
+		
+		System.out.println(double1 + double2);
+		
+		String string1 = "Hello WRLD";
+		
+		System.out.println(string1);
+		
+		char char1 = 'M';
+		char char2 = 'V';
+		
+		System.out.println(char1);
+		System.out.println(char2);
+		
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+		System.out.println(verdadeiro);
+		System.out.println(falso);
+		
+	}
+
+}
