9c541d6533460999d561ebda2f7986cbd4ec51ae
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-10-17 15:12:04 -0300

    Enviando a atividade da aula 02

---
+++ Aula 02/src/com/marcosviniciusdev/Main.java
@@ -0,0 +1,20 @@
+package com.marcosviniciusdev;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em celsius: ");
+        double celsius = sc.nextDouble();
+
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.printf("%.0f graus Celsius é igual a %.1f graus Fahrenheit.", celsius, fahrenheit);
+
+        sc.close();
+    }
+
+}
